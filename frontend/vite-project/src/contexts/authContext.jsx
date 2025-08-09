570c8eefae5e3b51ce1ac8ab6f039ae2ec9b0ed8
avinish321 <avinish729@gmail.com>
2025-08-10 04:56:51 +0530

    DEPLOYMENT FRONTEND

--- frontend/vite-project/src/contexts/authContext.jsx
+++ frontend/vite-project/src/contexts/authContext.jsx
@@ -2,11 +2,11 @@ import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import httpStatus from "http-status";
-
+import server from "../environment";
 export const AuthContext = createContext({});
 
 export const client = axios.create({
-  baseURL: "http://localhost:3000/api/v1/users",
+  baseURL: `${server}/api/v1/users`,
 });
 export const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
@@ -41,15 +41,12 @@ export const AuthProvider = ({ children }) => {
   };
 
   const addToUserHistory = async (meetingCode) => {
-    try {
+    
       let request = await client.post("/add_to_activity", {
         token: localStorage.getItem("token"),
         meeting_code: meetingCode,
       });
       return request.data;
-    } catch (err) {
-      throw err;
-    }
   };
 
   const value = {
---
+++ frontend/vite-project/src/environment.js
@@ -0,0 +1,7 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+      "https://avi-zoom-5.onrender.com":
+    "https://localhost:5173" 
+   
+
+export default server;
